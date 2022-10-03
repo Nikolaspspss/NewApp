dd8bd70fd165f9a71cca2184e28df0dc5b35575d
miumiu <pavlovNP1992@yandex.ru>
2022-10-03 17:51:19 +0300

    1

--- NewApp/new/views.py
+++ NewApp/new/views.py
@@ -1,6 +1,8 @@
 
 from django.views.generic import ListView, DetailView
 from .models import Post
+from datetime import datetime
+
 
 class PostList(ListView):
     # Указываем модель, объекты которой мы будем выводить
@@ -13,6 +15,19 @@ class PostList(ListView):
     # Это имя списка, в котором будут лежать все объекты.
     # Его надо указать, чтобы обратиться к списку объектов в html-шаблоне.
     context_object_name = 'Post'
+
+    def get_context_data(self, **kwargs):
+        # С помощью super() мы обращаемся к родительским классам
+        # и вызываем у них метод get_context_data с теми же аргументами,
+        # что и были переданы нам.
+        # В ответе мы должны получить словарь.
+        context = super().get_context_data(**kwargs)
+        # К словарю добавим текущую дату в ключ 'time_now'.
+        context['time_now'] = datetime.utcnow()
+        # Добавим ещё одну пустую переменную,
+        # чтобы на её примере рассмотреть работу ещё одного фильтра.
+        context['next_sale'] = 'Новости скора будут'
+        return context
 
 class PostDetail(DetailView):
     # Модель всё та же, но мы хотим получать информацию по отдельному товару
